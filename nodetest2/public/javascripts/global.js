2e106307798852aa693dfb494825d5c4ecbdcfb4
Elin <elinnet@gmail.com>
2015-11-02 21:46:33 +0000

    user info shows when clicked

--- nodetest2/public/javascripts/global.js
+++ nodetest2/public/javascripts/global.js
@@ -1,35 +1,47 @@
-// Userlist data array for filling in info box
+
+
 var userListData = [];
 
-// DOM Ready =============================================================
-$(document).ready(function() {
-
-    // Populate the user table on initial page load
-    populateTable();
-
+$(document).ready(function(){
+  populateTable();
+	$('#userList').on('click', 'td a.linkshowuser', showUserInfo);
 });
 
-// Functions =============================================================
+function populateTable(){
+  var tableContent = '';
 
-// Fill table with data
-function populateTable() {
+	$.getJSON('/users/userlist', function(data){
+	  userListData = data;
+	  $.each(data, function(){
+		  tableContent += '<tr>';
+		  tableContent += '<td><a href="#" class="linkshowuser" rel="' +
+			                 this.username + '" title="Show details">' +
+											 this.username + '</td>';
+		  tableContent += '<td>' + this.email + '</td>';
+		  tableContent += '<td><a href="#" class="linkdeleteuser" rel="' +
+			                 this._id + '">delete</a></td>';
+		  tableContent += '</tr>';
+	  });
+		$('#userList table tbody').html(tableContent);
+	});
+}
 
-    // Empty content string
-    var tableContent = '';
+function showUserInfo(event){
+  event.preventDefault();
+	var thisUser = $(this).attr('rel');
+	var arrayPosition = userListData.map(function(arrayItem){
+	  return arrayItem.username;
+	}).indexOf(thisUser);
 
-    // jQuery AJAX call for JSON
-    $.getJSON( '/users/userlist', function( data ) {
 
-        // For each item in our JSON, add a table row and cells to the content string
-        $.each(data, function(){
-            tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-            tableContent += '</tr>';
-        });
+    // Get our User Object
+    var thisUserObject = userListData[arrayPosition];
 
-        // Inject the whole content string into our existing HTML table
-        $('#userList table tbody').html(tableContent);
-    });
-};
+    //Populate Info Box
+    $('#userInfoName').text(thisUserObject.fullname);
+    $('#userInfoAge').text(thisUserObject.age);
+    $('#userInfoGender').text(thisUserObject.gender);
+    $('#userInfoLocation').text(thisUserObject.location);
+
+
+}
